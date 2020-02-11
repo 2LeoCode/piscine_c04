f07bf77a8669df8edc0b75a018e919aef865056e
Your Name <you@example.co>
2020-02-11 10:10:58 +0100

    yuuuuu

--- ex01/ft_putnbr.c
+++ ex01/ft_putnbr.c
@@ -6,69 +6,30 @@
 /*   By: lsuardi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 16:36:22 by lsuardi           #+#    #+#             */
-/*   Updated: 2020/02/10 16:41:11 by lsuardi          ###   ########.fr       */
+/*   Updated: 2020/02/11 10:07:00 by lsuardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int		ft_nbchiffres(int i)
-{
-	int n;
-
-	n = 0;
-	while (i != 0)
-	{
-		i /= 10;
-		n++;
-	}
-	return (n);
-}
-
-int		ft_is_negative(int i)
-{
-	int j;
-
-	if (i < 0)
-	{
-		j = -1;
-	}
-	else
-	{
-		j = 1;
-	}
-	return (j);
-}
-
-void	ft_print_ng(int i)
-{
-	if (i < 0)
-	{
-		write(1, "-", 1);
-	}
-}
-
 void	ft_putnbr(int nb)
 {
-	int		cpt;
-	int		index;
-	char	tab_ch[10];
+	int i;
 
-	cpt = ft_nbchiffres(nb);
-	index = 1;
-	ft_print_ng(nb);
-	while (cpt > 1)
+	i = nb;
+	if (nb < 0)
 	{
-		index *= 10;
-		cpt--;
+		write(1, '-', 1);
+		if (i == -2147483648)
+			i = 2147483647;
+		else
+			i *= -1;
 	}
-	cpt = 0;
-	while (index > 0)
+	if (nb > 9)
 	{
-		tab_ch[cpt] = (char)(nb / index * ft_is_negative(nb)) + 48;
-		write(1, &tab_ch[cpt], 1);
-		nb %= index;
-		index /= 10;
-		cpt++;
+		ft_putnbr(i / 10);
+		ft_putnbr(i % 10);
 	}
+	else
+		write(1, i + 48, 1);
 }
