44682926eae57bdd5b299985213bf78f23ef9d24
Your Name <you@example.co>
2020-02-17 23:03:06 +0100

    lolmdr

--- ex05/ft_atoi_base.c
+++ ex05/ft_atoi_base.c
@@ -6,9 +6,47 @@
 /*   By: lsuardi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 13:10:33 by lsuardi           #+#    #+#             */
-/*   Updated: 2020/02/12 14:42:48 by lsuardi          ###   ########.fr       */
+/*   Updated: 2020/02/17 23:01:58 by lsuardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int		ft_isspace(char c)
+{
+	if (c == '\t' || c == '\n' || c == '\v')
+		return (1);
+	if (c == '\f' || c == '\r' || c == ' ')
+		return (1);
+	return (0);
+}
+
+int		ft_atoi(char *str)
+{
+	int i;
+	int nb;
+	int neg;
+
+	i = 0;
+	nb = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	i--;
+	while (str[++i] == '+' || str[i] == '-')
+		if (str[i] == '-')
+			neg++;
+	if (neg % 2 == 0)
+		neg = -1;
+	else
+		neg = 1;
+	i--;
+	if (str[i] >= '0' && str[i] <= '9')
+		nb = str[i] - 48;
+	while (str[++i] >= '0' && str[i] <= '9')
+	{
+		nb *= 10;
+		nb += str[i] - 48;
+	}
+	return (nb * neg);
+}
 
 int		ft_calc_base(char *b)
 {
@@ -30,39 +68,31 @@ int		ft_calc_base(char *b)
 	return(i);
 }
 
-int		ft_nbr(char *str)
+void	ft_compare_string_base(char *string, char *base)
 {
 	int i;
-	
-	i = -1;
-	while (str[++i]);
-	if(str[0] == '-')
-		return (i);
-	else
-		return (i + 1)
-}
+	int j;
 
-void	ft_negative(char *str, int *nbr, int *isneg)
-{
-	if (str[0] == '-')
-		isneg
+	i = -1;
+	while (base[++i])
+	{
+		j = -1;
+		while (string[++j])
+			if (string[j] == base[i])
+				string[j] = i + 48;
+	}
 }
 
 int		ft_atoi_base(char *str, char *base)
 {
-	int nb_int;
-	int i;
+	int nb;
+	int bs;
 	int j;
 
-	i = ft_calc_base(base);
-	j = ft_nbr(str);
-	while (--j >= 0)
-	{
-		if (!(j == 0 && str[0] == '-'))
-			{
-				if ()
-				nb_int += j * 10;	
-			}
-	}
+	bs = ft_calc_base(base);
+	if (bs == 0)
+		return (0);
+	ft_compare_string_base(str, base);
+	while ()	
 	return (nb_int);
 }
